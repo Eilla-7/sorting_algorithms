b49503fd8db3aa5f326bd60f04f5c88c516dd61c
Eilla-7 <eilafadel2238@gmail.com>
2023-12-19 10:30:55 -0800

    rename

---
+++ tryme.c
@@ -0,0 +1,56 @@
+/**
+ * return_head - function to return the head of the list
+ * @listt: the list
+ *
+ * Return: the head of the list
+ */
+listint_t *return_head(listint_t *listt)
+{
+	while (listt && listt->prev != NULL)
+		listt = listt->prev;
+
+	return listt;
+}
+
+
+
+/**
+ * insertion_sort_list - function that uses insertion sort to sort the list
+ * @list: list to be sorted
+ *
+ * Return: void
+ */
+void insertion_sort_list(listint_t **list)
+{
+	if (!list || !(*list) || !(*list)->next)
+		return;
+
+	listint_t *temp1, *temp2, *spot;
+	temp1 = return_head(*list);
+
+	for (temp1 = temp1->next; temp1; temp1 = spot)
+	{
+		spot = temp1->next;
+
+		while (temp1->prev != NULL && temp1->prev->n > temp1->n)
+		{
+			temp2 = temp1->prev;
+
+			if (temp2->prev != NULL)
+				temp2->prev->next = temp1;
+			if (temp1->next != NULL)
+				temp1->next->prev = temp2;
+
+			temp1->prev = temp2->prev;
+			temp2->next = temp1->next;
+			temp1->next = temp2;
+			temp2->prev = temp1;
+
+			if (temp1->prev == NULL)
+				*list = temp1;
+
+			print_list(return_head(*list));
+		}
+	}
+}
+
