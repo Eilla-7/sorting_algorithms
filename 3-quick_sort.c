699ec13280ae812002218ff53c97322ad01f4f96
MemanAwad <meman1awad@gmail.com>
2023-12-19 22:59:05 +0200

    quick sort algorithm

---
+++ 3-quick_sort.c
@@ -0,0 +1,54 @@
+#include "sort.h"
+
+void swap(int *a, int *b)
+{
+	int temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
+int lomuto_partition(int *array, int low, int high, size_t size)
+{
+	int pv_index = low;
+	int i = low;
+	int pivot = array[high];
+	int temp;
+
+	for (i = low; i < high; i++)
+	{
+		if (array[i] <= pivot)
+		{
+			swap(&array[i], &array[pv_index]);
+			if (pv_index != i)
+				print_array(array, size);
+			pv_index++;
+		}
+	}
+	temp = array[pv_index];
+	array[pv_index] = pivot;
+	array[high] = temp;
+	if (pv_index != high)
+		print_array(array, size);
+	return pv_index;
+}
+
+void sort_helper(int *array, int lower, int higher, size_t size)
+{
+	int pv_index;
+
+	if (lower < higher) {
+		pv_index = lomuto_partition(array, lower, higher, size);
+
+		sort_helper(array, lower, pv_index - 1, size);
+		sort_helper(array, pv_index + 1, higher, size);
+	}
+}
+
+void quick_sort(int *array, size_t size)
+{
+	if (array == NULL || size <= 1) {
+		return; 
+
+	}
+	sort_helper(array, 0, size - 1, size);
+}
