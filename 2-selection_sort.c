d4b967f7fcf7692a49459fefddc2d53973d09c74
Eilla-7 <eilafadel2238@gmail.com>
2023-12-19 05:51:15 -0800

    selection

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -15,10 +15,10 @@ void selection_sort(int *array, size_t size)
 
 	if (!array || !size)
 		return;
-	for (i = 0; i < size; i++)
+	for (i = 0; i < size; ++i)
 	{
 		min = i;
-		for (j = i + 1; j < size; j++)
+		for (j = i + 1; j < size; ++j)
 		{
 			if (array[j] < array[min])
 			{
