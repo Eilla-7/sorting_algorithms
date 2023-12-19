ca16bfd514b15d170c0cc13e9c72fcb5b2201ab6
Eilla-7 <eilafadel2238@gmail.com>
2023-12-19 05:41:18 -0800

    selection

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -23,12 +23,12 @@ void selection_sort(int *array, size_t size)
 				min = j;
 			}
 		}
-		if (min != i)
-		{
-			tmp = array[min];
-			array[min] = array[i];
-			array[i] = tmp;
-		}
+
+
+		tmp = array[min];
+		array[min] = array[i];
+		array[i] = tmp;
+
 		print_array(array, size);
 	}
 }
