709fd1840c5d1634def5a52f9ab672fc38aef553
MemanAwad <meman1awad@gmail.com>
2023-12-19 23:37:16 +0200

    quick sorrt algorithm

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -29,25 +29,31 @@ int lomuto_partition(int *array, int low, int high, size_t size)
 	array[high] = temp;
 	if (pv_index != high)
 		print_array(array, size);
-	return pv_index;
+	return (pv_index);
 }
 
 void sort_helper(int *array, int lower, int higher, size_t size)
 {
 	int pv_index;
 
-	if (lower < higher) {
+	if (lower < higher)
+	{
 		pv_index = lomuto_partition(array, lower, higher, size);
 
 		sort_helper(array, lower, pv_index - 1, size);
 		sort_helper(array, pv_index + 1, higher, size);
 	}
 }
-
+/**
+ * quick_sort - function use quicj=k sort to sort the array
+ * @array: the array to be sorted
+ * @size: the sixe of the array to be sorted
+ */
 void quick_sort(int *array, size_t size)
 {
-	if (array == NULL || size <= 1) {
-		return; 
+	if (array == NULL || size <= 1)
+	{
+		return;
 
 	}
 	sort_helper(array, 0, size - 1, size);
